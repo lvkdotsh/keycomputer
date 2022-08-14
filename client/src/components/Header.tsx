74aa397b5c13296f6f9beb3762dbe19ed24acade
Lucemans <luc@lucemans.nl>
2022-08-14 10:10:11 +0200

    Add Credits

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,10 @@
-import { FC } from "react";
-import { Link } from "react-router-dom";
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Header: FC = () => {
     return (
-        <div className="w-full py-4 px-8 border-b border-b-yellow-500">
-            <div className="bg-neutral-800 text-white w-fit font-bold border-yellow-500 border-2">
+        <div className="w-full py-4 px-8 border-b border-b-yellow-500 flex justify-between">
+            <div className="bg-neutral-800 text-white w-fit font-bold border-yellow-500 border-2 hover:brightness-90">
                 <Link to="/" className="cursor-pointer">
                     <div className="border-r-2 border-r-yellow-500 py-2 inline-block px-2">
                         <span className="hover:rotate-90">🔑</span>
@@ -12,6 +12,17 @@ export const Header: FC = () => {
                     <span className="inline-block px-2 py-2">key.computer</span>
                 </Link>
             </div>
+            <a
+                href="https://twitter.com/lucemansnl"
+                className="border-yellow-500 bg-neutral-800 px-4 py-1 hover:brightness-90 flex border-2 items-center justify-center w-fit"
+            >
+                A Lucemans Site
+                <img
+                    className="w-8 rounded-full"
+                    src="https://header.luc.computer/public/500x500.webp"
+                    alt=""
+                />
+            </a>
         </div>
     );
 };
