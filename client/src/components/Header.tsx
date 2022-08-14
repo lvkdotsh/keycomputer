83546b996aaffb41ff331af491d73bc787e59dfd
Lucemans <luc@lucemans.nl>
2022-08-14 10:21:56 +0200

    Fix Header Padding

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export const Header: FC = () => {
     return (
-        <div className="w-full py-4 px-8 border-b border-b-yellow-500 flex justify-between items-center">
+        <div className="w-full py-4 px-4 border-b border-b-yellow-500 flex justify-between items-center">
             <div className="bg-neutral-800 text-white w-fit font-bold border-yellow-500 border-2 hover:brightness-90">
                 <Link to="/" className="cursor-pointer">
                     <div className="border-r-2 border-r-yellow-500 py-2 inline-block px-2">
