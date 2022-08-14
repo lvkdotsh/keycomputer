135aac61243ac25c4eb9c7c2333e3de8e0df906d
Lucemans <luc@lucemans.nl>
2022-08-14 16:58:01 +0200

    Improve image rounding

--- client/src/components/KeyPreviewCard.tsx
+++ client/src/components/KeyPreviewCard.tsx
@@ -1,25 +1,27 @@
-import { FC } from "react";
-import { Link } from "react-router-dom";
-import { KeyType } from "../../../server/src/types/KeyType";
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+import { KeyType } from '../../../server/src/types/KeyType';
 
 export const KeyPreviewCard: FC<{ data: KeyType }> = ({ data }) => {
     return (
-        <div className="w-full bg-neutral-800 p-2 hover:bg-neutral-700 rounded-md">
-            <Link
-                to={`/key/${data.slug}`}
-                className="block cursor-pointer w-full bg-neutral-400"
-                style={{ aspectRatio: "3 / 4" }}
-            >
-                <img
-                    src={`/set/preview/${data.slug}.webp`}
-                    className="w-full h-full object-cover rounded-md"
-                    style={{ aspectRatio: "3 / 4" }}
-                    alt={data.slug}
-                />
-            </Link>
-            <div className="flex flex-col items-center px-2 py-2 mt-2">
-                <div className="font-bold">{data.name}</div>
+        <Link to={`/key/${data.slug}`}>
+            <div className="w-full bg-neutral-800 p-2 hover:bg-neutral-700 rounded-md">
+                <div
+                    className="block cursor-pointer w-full bg-neutral-400 rounded-md overflow-hidden"
+                    style={{ aspectRatio: '3 / 4' }}
+                >
+                    <img
+                        src={`/set/preview/${data.slug}.webp`}
+                        className="w-full h-full object-cover rounded-md"
+                        style={{ aspectRatio: '3 / 4' }}
+                        alt={data.slug}
+                    />
+                </div>
+                <div className="flex flex-col items-center px-2 py-2 mt-2">
+                    <div className="font-bold">{data.name}</div>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 };
